2ebc309bb6018429ed87ba6c99f69c5ac661ea9c
dmdm-abs <dmdm@abstractit.in>
2023-03-07 13:15:07 +0530

    Refactored

--- src/components/MarkSheets/Input.js
+++ src/components/MarkSheets/Input.js
@@ -1,19 +1,17 @@
-import { peek } from '@laufire/utils/debug';
 import React from 'react';
 
 const Input = (context) => {
-	const { actions,
+	const { actions: { setValue },
 		config: { currentMarkSheet }
 		, data: { input, key }} = context;
 
-	peek(key, 'key-header');
 	return <td>
 		<input
 			type={ currentMarkSheet[key].type }
 			value={ input }
 			className="input"
 			onChange={ ({ target: { value }}) =>
-				actions.setValue({ key: key,
+				setValue({ key: key,
 					value: currentMarkSheet[key].convertor(value) }) }
 		/>
 	</td>;
